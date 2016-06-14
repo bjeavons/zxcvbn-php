1ca726d48356b9d661844f684dcdf94c94a89552
George Cooksey <texdc3@gmail.com>
2016-06-13 21:04:57 -0500

    Add test namespace

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,8 +5,3 @@ if (!file_exists($autoloadFile)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
 require_once $autoloadFile;
-
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->add('ZxcvbnPhp\Test', 'test');
-$loader->register();
-
