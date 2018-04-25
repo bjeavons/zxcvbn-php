237847aa787a26bf47ba98bf5db49966e052fa78
Michael Kopinsky <Michael.Kopinsky@uphs.upenn.edu>
2018-04-25 02:02:41 -0400

    Add remaining L33t tests

--- test/Matchers/L33tTest.php
+++ test/Matchers/L33tTest.php
@@ -43,6 +43,8 @@ class L33tTest extends AbstractMatchTest
 
     public function testReducesL33tTable()
     {
+        // As this is truly a unit test testing the internals, I'm OK with not implementing
+        // it unless it's helpful for resolving discrepancies between the libraries
         $this->markTestSkipped('Not implemented');
 
         $cases = [
@@ -70,6 +72,23 @@ class L33tTest extends AbstractMatchTest
                 "reduces l33t table to only the substitutions that a password might be employing"
             );
         }
+    }
+
+    public function testEnumeratesL33tSubstitutions()
+    {
+        // As this is truly a unit test testing the internals, I'm OK with not implementing
+        // it unless it's helpful for resolving discrepancies between the libraries
+        $this->markTestSkipped('Not implemented');
+
+        // CoffeeScript source: 
+        // for [table, subs] in [
+        //   [ {},                        [{}] ]
+        //   [ {a: ['@']},                [{'@': 'a'}] ]
+        //   [ {a: ['@','4']},            [{'@': 'a'}, {'4': 'a'}] ]
+        //   [ {a: ['@','4'], c: ['(']},  [{'@': 'a', '(': 'c' }, {'4': 'a', '(': 'c'}] ]
+        //   ]
+        //   msg = "enumerates the different sets of l33t substitutions a password might be using"
+        //   t.deepEquals matching.enumerate_l33t_subs(table), subs, msg
     }
 
     public function testEmptyString()
@@ -138,5 +157,65 @@ class L33tTest extends AbstractMatchTest
                 ]
             );
         }
+    }
+
+    public function testOverlappingL33tPatterns()
+    {
+        $this->checkMatches(
+            "matches against overlapping l33t patterns",
+            MockL33tMatch::match('@a(go{G0'),
+            'dictionary',
+            ['@a(', '(go', '{G0'],
+            [[0,2], [2,4], [5,7]],
+            [
+                'l33t'           => [true, true, true],
+                'sub'            => [
+                                        ['@' => 'a', '(' => 'c'],
+                                        ['(' => 'c'],
+                                        ['{' => 'c', '0' => 'o']
+                                    ],
+                'matchedWord'    => ['aac', 'cgo', 'cgo'],
+                'rank'           => [1, 1, 1],
+                'dictionaryName' => ['words', 'words2', 'words2'],
+            ]
+        );
+    }
+
+    public function testMultipleL33tSubstitutions()
+    {
+        $this->assertEquals(
+            [],
+            MockL33tMatch::match('p4@ssword'),
+            "doesn't match when multiple l33t substitutions are needed for the same letter"
+        );
+    }
+
+    public function testSingleCharacterL33tWords()
+    {
+        $this->assertEquals(
+            [],
+            MockL33tMatch::match('4 1 @'),
+            "doesn't match single-character l33ted words"
+        );
+    }
+
+    public function testSubstitutionSubsets()
+    {
+        // From the coffeescript source:
+        // 
+        // # known issue: subsets of substitutions aren't tried.
+        // # for long inputs, trying every subset of every possible substitution could quickly get large,
+        // # but there might be a performant way to fix.
+        // # (so in this example: {'4': a, '0': 'o'} is detected as a possible sub,
+        // # but the subset {'4': 'a'} isn't tried, missing the match for asdf0.)
+        // # TODO: consider partially fixing by trying all subsets of size 1 and maybe 2
+        // msg = "doesn't match with subsets of possible l33t substitutions"
+        // t.deepEqual lm('4sdf0'), [], msg
+
+        $this->assertEquals(
+            [],
+            MockL33tMatch::match('4sdf0'),
+            "doesn't match with subsets of possible l33t substitutions"
+        );
     }
 }
