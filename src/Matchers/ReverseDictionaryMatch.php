82c1037d083309f1f5842dd2c64630e0c23a74ef
Sam Horn <samuel.horn1@gmail.com>
2018-06-14 20:35:40 +1000

    Implemented reverse directionary match

---
+++ src/Matchers/ReverseDictionaryMatch.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ZxcvbnPhp\Matchers;
+
+use ZxcvbnPhp\Matcher;
+
+class ReverseDictionaryMatch extends DictionaryMatch
+{
+    public $reversed = true;
+
+    /**
+     * Match occurences of reversed dictionary words in password.
+     *
+     * @inheritdoc
+     */
+    public static function match($password, array $userInputs = array(), $rankedDictionaries = null)
+    {
+        $matches = parent::match(strrev($password), $userInputs, $rankedDictionaries);
+        /** @var ReverseDictionaryMatch $match */
+        foreach ($matches as $match) {
+            $match->reversed = true;
+            $tempBegin = $match->begin;
+
+            $match->token = strrev($match->token);
+            $match->password = strrev($match->password);
+            $match->begin = strlen($password) - 1 - $match->end;
+            $match->end = strlen($password) - 1 - $tempBegin;
+        }
+
+        usort($matches, [Matcher::class, 'sortMatches']);
+        return $matches;
+    }
+}
